def9ed772a2dd62cd0e179707eb59ecc299cf98a
pdkainth <kainthprabhdeep9162@students.esuhsd.org>
2017-12-23 18:28:50 -0800

    Added idle, encoder, and manual control for lever arm

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LeverArm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LeverArm.java
@@ -10,6 +10,10 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 public class LeverArm {
 
+  public enum MotorOpMode {
+    IDLE, MANUAL, ENCODER
+  }
+
   private final static double LEVER_MOTOR_POWER = 0.75;
   private final static int encoderPosition[] = {0, 600, 3600, 6600, 9300};
 
@@ -17,6 +21,7 @@ public class LeverArm {
 
   private int targetPositioIndex;
   private int targetPosition;
+  private MotorOpMode opMode = MotorOpMode.IDLE;
 
   public void init(HardwareMap hardwaremap) {
     leverArmDrive = hardwaremap.get(DcMotor.class, "Motor5");
@@ -26,40 +31,58 @@ public class LeverArm {
     resetEncoder();
   }
 
+  public MotorOpMode getOpState() {
+    return opMode;
+  }
+
   public void resetEncoder() {
     leverArmDrive.setPower(0.0);
     leverArmDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
     targetPosition = 0;
     targetPositioIndex = 0;
     leverArmDrive.setTargetPosition(targetPosition);
+    opMode = MotorOpMode.IDLE;
   }
 
   public void raise(double raise, Telemetry telemetry) {
+    opMode = MotorOpMode.MANUAL;
+    raise = raise * LEVER_MOTOR_POWER;
+    leverArmDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
     leverArmDrive.setPower(raise);
+
     int encoderPosition = leverArmDrive.getCurrentPosition();
-    telemetry.addData("leverArm", "power %.2f position %d", raise, encoderPosition);
+    telemetry.addData("leverArm", "pow %.2f pos %d", raise, encoderPosition);
   }
 
-  public void setPosition(int position, Telemetry telemetry){
-    if ((position >= 0) && (position <= 4)){
+  public void setPosition(int position, Telemetry telemetry) {
+    int curEncoderPosition;
+    boolean newPositionValid = false;
+
+    newPositionValid = (position >= 0) && (position <= 4);
+    curEncoderPosition = leverArmDrive.getCurrentPosition();
+
+    if (newPositionValid) {
+      opMode = MotorOpMode.ENCODER;
+      leverArmDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
       targetPositioIndex = position;
       targetPosition = encoderPosition[position];
       leverArmDrive.setTargetPosition(targetPosition);
       leverArmDrive.setPower(LEVER_MOTOR_POWER);
     }
 
-    int encoderPosition = leverArmDrive.getCurrentPosition();
     boolean motorBusy = leverArmDrive.isBusy();
+    boolean withinTargetRange = Math.abs(curEncoderPosition - targetPosition) < 10;
 
-    if ((Math.abs(encoderPosition - targetPosition) < 10) && (motorBusy == false)) {
-      leverArmDrive.setPower(0.0);
+    if ((opMode == MotorOpMode.ENCODER) && withinTargetRange && motorBusy) {
+      stop();
     }
 
-    telemetry.addData("leverArm", "motorBusy %b targetPositioIndex %d CurrentPos %d TargetPos %d",
-      motorBusy, targetPositioIndex, encoderPosition, targetPosition);
+    telemetry.addData("leverArm", "position:idx %d cur %d tgt %d busy %b",
+      targetPositioIndex, curEncoderPosition, targetPosition, motorBusy);
   }
 
   public void stop() {
+    opMode = MotorOpMode.IDLE;
     leverArmDrive.setPower(0.0);
   }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MyTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MyTeleOp.java
@@ -69,56 +69,71 @@ public class MyTeleOp extends OpMode {
     double drive = -gamepad1.left_stick_y;
     double strafe = 0.0;
 
-    if (gamepad1.dpad_left == true){
+    if (gamepad1.dpad_left == true) {
       strafe = -1.0;
-    } else if (gamepad1.dpad_right == true){
+    } else if (gamepad1.dpad_right == true) {
       strafe = 1.0;
     }
 
     double rotate = gamepad1.right_stick_x;
 
     boolean override = (gamepad1.left_trigger > 0.7);
-    wheels.drive(drive, strafe, rotate, telemetry, distanceRange,override);
+    wheels.drive(drive, strafe, rotate, telemetry, distanceRange, override);
     telemetry.addData("gamepad1 wheels", "D(%.2f) S(%.2f) R(%.2f)", drive, strafe, rotate);
   }
 
   private void updateArm1() {
+    int position;
     boolean position0 = gamepad1.right_bumper;
     boolean position1 = gamepad1.y;
     boolean position2 = gamepad1.b;
     boolean position3 = gamepad1.a;
     boolean position4 = gamepad1.x;
 
+    double raise = 0.0;
+    if (gamepad1.dpad_up) {
+      raise = 1.0;
+    }
+    if (gamepad1.dpad_down) {
+      raise = -1.0;
+    }
 
     if (gamepad1.start) {
       arm1.resetEncoder();
+    } else if (raise != 0) {
+      arm1.raise(raise, telemetry);
     } else {
-      if (position0 == true) {
-        arm1.setPosition(0, telemetry);
-      } else if (position1 == true) {
-        arm1.setPosition(1, telemetry);
-      } else if (position2 == true) {
-        arm1.setPosition(2, telemetry);
-      } else if (position3 == true) {
-        arm1.setPosition(3, telemetry);
-      } else if (position4 == true) {
-        arm1.setPosition(4, telemetry);
-      } else {
-        arm1.setPosition(-1, telemetry);
+      if (arm1.getOpState() == LeverArm.MotorOpMode.MANUAL) {
+        arm1.stop();
       }
+
+      if (position0 == true) {
+        position = 0;
+      } else if (position1 == true) {
+        position = 1;
+      } else if (position2 == true) {
+        position = 2;
+      } else if (position3 == true) {
+        position = 3;
+      } else if (position4 == true) {
+        position = 4;
+      } else {
+        position = -1;
+      }
+      arm1.setPosition(position, telemetry);
     }
   }
 
   private void testArm2() {
-    double positionLeft  = gamepad2.left_stick_x;
+    double positionLeft = gamepad2.left_stick_x;
     double positionRight = gamepad2.right_stick_x;
     arm2.update(positionLeft, positionRight, telemetry);
   }
 
-  private void updateArm2(){
+  private void updateArm2() {
     ButtonState buttonCond;
 
-    if (gamepad1.left_bumper == true){
+    if (gamepad1.left_bumper == true) {
       buttonCond = ButtonState.PRESSED;
     } else {
       buttonCond = ButtonState.RELEASED;
